ea0af055a139ad436938cac9213dc18cb9a70db6
hone <hone@microsoft.com>
2022-02-04 01:52:46 +0530

    redux addeD

--- Screens/History.js
+++ Screens/History.js
@@ -1,17 +1,61 @@
-import * as React from 'react';
+import React,{useEffect,useState} from 'react';
 import {View, Text} from 'react-native';
 import {Searchbar} from 'react-native-paper';
+import EncryptedStorage from 'react-native-encrypted-storage';
 
-function History() {
-  const [searchQuery, setSearchQuery] = React.useState('');
-  const onChangeSearch = query => setSearchQuery(query);
 
+async function storeUserSession() {
+    try {
+        await EncryptedStorage.setItem(
+            "London",
+            JSON.stringify({
+                temp_c : 29,
+                temp_f : 85,
+                 localtime:'3-feb-2020',
+                                  "wind_mph": 25,
+                                  "wind_kph": 30,
+
+            })
+        );
+
+        // Congrats! You've just stored your first value!
+    } catch (error) {
+        // There was an error on the native side
+    }
+}
+
+const day = (city)=>{
+const [dayOfMonth, setDayOfMonth] = useState('')
+
+useEffect(() => {
+
+async function retrieveUserSession() {
+    try {
+
+        const session = await EncryptedStorage.getItem(city);
+
+        if (session !== undefined) {
+            // Congrats! You've just retrieved your first value!
+           // console.log(session)
+         setDayOfMonth(JSON.parse(session).temp_c);
+        }
+    } catch (error) {
+        // There was an error on the native side
+    }
+}
+retrieveUserSession()},[dayOfMonth])
+
+return dayOfMonth;
+}
+
+function History({route, navigation }) {
+storeUserSession();
+const { cityName } = route.params;
+const value = day(cityName);
   return (
-    <Searchbar
-      placeholder="Enter the name of the city"
-      onChangeText={onChangeSearch}
-      value={searchQuery}
-    />
+   <Text style={{fontSize: 15, textAlign: 'center'}}>
+{value}
+   </Text>
   );
 }
 

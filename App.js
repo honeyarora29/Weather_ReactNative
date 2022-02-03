ab49184949750263e03ab8bd176270a000a46f27
hone <hone@microsoft.com>
2022-02-04 01:09:47 +0530

    redux addeD

--- App.js
+++ App.js
@@ -4,17 +4,23 @@ import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import HomeScreen from './Screens/HomeScreen';
 import SearchScreen from './Screens/SearchScreen';
+import History from './Screens/History';
+import {Provider} from 'react-redux';
+import {store} from './store/Store';
 
 const Stack = createNativeStackNavigator();
 
 function App() {
   return (
+  <Provider store={store}>
     <NavigationContainer>
       <Stack.Navigator initialRouteName="WeatherApp">
         <Stack.Screen name="WeatherApp" component={HomeScreen} />
         <Stack.Screen name="Search" component={SearchScreen} />
+        <Stack.Screen name="History" component={History} />
       </Stack.Navigator>
     </NavigationContainer>
+    </Provider>
   );
 
 }
---
+++ Screens/History.js
@@ -0,0 +1,18 @@
+import * as React from 'react';
+import {View, Text} from 'react-native';
+import {Searchbar} from 'react-native-paper';
+
+function History() {
+  const [searchQuery, setSearchQuery] = React.useState('');
+  const onChangeSearch = query => setSearchQuery(query);
+
+  return (
+    <Searchbar
+      placeholder="Enter the name of the city"
+      onChangeText={onChangeSearch}
+      value={searchQuery}
+    />
+  );
+}
+
+export default History;
